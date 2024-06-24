2255d85344ab65b47af334e30573dd2dd1998801
Jimmy Bradshaw <james.g.bradshaw@gmail.com>
2024-06-23 20:10:54 -0500

    refactor a bit now that we have tests!

--- src/utils.ts
+++ src/utils.ts
@@ -25,7 +25,6 @@ const dateFormatter = timeFormat('%Y-%m-%d')
 const convertLibbyImportItem = (libbyItem: LibbyImportItem) => {
   const activityDate = dateParser(libbyItem.timestamp)
   if (!activityDate) return
-  if (libbyItem['activity'] !== 'Borrowed' && libbyItem['activity'] !== 'Returned') return
   const formattedActivityDate = dateFormatter(activityDate)
   const goodReadsItem: GoodreadsExportItem = {
     'Title': libbyItem['title'],
@@ -33,42 +32,65 @@ const convertLibbyImportItem = (libbyItem: LibbyImportItem) => {
     'Publisher': libbyItem['publisher'],
     'ISBN': libbyItem['isbn'],
   }
-  if (libbyItem['activity'] === 'Borrowed' && libbyItem['details'] !== '') {
-    goodReadsItem['Shelves'] = 'currently-reading'
-    goodReadsItem['Date Added'] = formattedActivityDate
-  }
-  if (libbyItem['activity'] === 'Returned' || (libbyItem['activity'] === 'Borrowed' && libbyItem['details'] === '')) {
-    goodReadsItem['Shelves'] = 'read'
-    goodReadsItem['Date Read'] = formattedActivityDate
+  switch (libbyItem['activity']) {
+    case 'Borrowed':
+      if (libbyItem['details'] === '') {
+        goodReadsItem['Shelves'] = 'read'
+        goodReadsItem['Date Read'] = formattedActivityDate
+      } else {
+        goodReadsItem['Shelves'] = 'currently-reading'
+        goodReadsItem['Date Added'] = formattedActivityDate
+      }
+      break;
+    case 'Returned':
+      goodReadsItem['Shelves'] = 'read'
+      goodReadsItem['Date Read'] = formattedActivityDate
+      break;
+    case 'Placed on hold':
+      goodReadsItem['Shelves'] = 'to-read'
+      goodReadsItem['Date Added'] = formattedActivityDate
+      break;
   }
   return goodReadsItem
 }
 
-export function isTruthy<T>(value?: T | undefined | null | false): value is T {
+function isTruthy<T>(value?: T | undefined | null | false): value is T {
   return !!value
 }
 
+function getCutoffDate(timeframe?: Timeframe): Date | undefined {
+  let cutoff: Date | undefined = new Date()
+  switch(timeframe) {
+    case 'all-time':
+      cutoff = undefined
+      break;
+    case 'last-year':
+      cutoff.setDate(cutoff.getDate() - 365)
+      break;
+    case 'last-month':
+      cutoff.setMonth(cutoff.getMonth() - 1)
+      break;
+    default:
+      cutoff = undefined
+  }
+  return cutoff
+}
+
 export const transformCSV = (libbyImportItems: LibbyImportItem[], timeframe?: Timeframe) => {
-    let cutoff: Date | undefined = new Date()
-    switch(timeframe) {
-      case 'all-time':
-        cutoff = undefined
-        break;
-      case 'last-year':
-        cutoff.setDate(cutoff.getDate() - 365)
-        break;
-      case 'last-month':
-        cutoff.setMonth(cutoff.getMonth() - 1)
-        break;
-      default:
-        cutoff = undefined
-    }
-    const filteredLibbyImportItems = libbyImportItems.filter((item) => {
+    const cutoff = getCutoffDate(timeframe)
+    const cutoffFilter = (item: LibbyImportItem) => {
       const timestamp = dateParser(item.timestamp)
       if (!timestamp) return false
       if (!cutoff) return true
       return timestamp > cutoff
+    }
+    const eventFilter = (item: LibbyImportItem) => {
+      return ['Returned', 'Borrowed', 'Placed on hold'].includes(item.activity)
+    }
+    const filteredLibbyImportItems = libbyImportItems.filter((item) => {
+      return cutoffFilter(item) && eventFilter(item)
     })
+
     const sortedLibbyImportItems = [...filteredLibbyImportItems].sort((a, b) => {
       const aTimestamp = dateParser(a.timestamp) as Date
       const bTimestamp = dateParser(b.timestamp) as Date
